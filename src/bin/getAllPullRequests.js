a0ab7901d57d865088145ba443daf1139e8de7dc
Adam <adkapx@gmail.com>
2019-09-30 17:38:47 -0400

    Update getAllPullIssues to modern git api

--- src/bin/getAllPullRequests.js
+++ src/bin/getAllPullRequests.js
@@ -3,13 +3,9 @@ module.exports = async (context, {owner, repo}) => {
   // method returns all the pull requests
   console.log('In getAllPullRequests.js...')
   let pullRequests = await context.github.paginate(
-    context.github.pullRequests.getAll({
-      owner,
-      repo,
-      state: 'all',
-      per_page: 100
-    }),
-    res => res.data
+    context.github.pulls.list.endpoint.merge(
+        context.repo({ state: 'all' })
+    )
   )
   return pullRequests
 }
